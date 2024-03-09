e70c150ad1269efb157df7b7280cf966938de083
Lalit kumar rout <routlalit172@gmail.com>
2024-03-09 15:26:26 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-
-import './App.css';
+import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <div>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_medium.jpg " alt="logo"/>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
